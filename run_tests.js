d2fc42d4b01514399ab32ac08ba04cb476a9635b
Sergey Zelenov <github@zelenov.su>
2018-11-09 18:30:48 +0100

    Update run_tests.js

--- run_tests.js
+++ run_tests.js
@@ -3,28 +3,28 @@ const glob = require('glob');
 
 module.exports = function runTests(moduleName, taskName, options) {
   if (!moduleName) {
-    console.error({
+    console.error(JSON.stringify({
       format_error: true,
       message: 'Required parameter `moduleName` is missing. Example: `npm test 0-module 1-task`'
-    });
+    }));
     process.exit(1);
   }
   
   if (!taskName) {
-    console.error({
+    console.error(JSON.stringify({
       format_error: true,
       message: 'Required parameter `taskName` is missing. Example: `npm test 0-module 1-task`'
-    });
+    }));
     process.exit(1);
   }
 
   const tests = glob.sync(`${moduleName}/${taskName}/test/**/**.test.js`);
   
   if (tests.length === 0) {
-    console.error({
+    console.error(JSON.stringify({
       format_error: true,
       message: `There are no test files in ${moduleName}/${taskName}. Please check module and task.`
-    });
+    }));
     process.exit(1);
   }
   
